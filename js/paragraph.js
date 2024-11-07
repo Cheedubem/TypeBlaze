dee8f39cda564d955b7f702d2f46f47de3f38028
Cheedubem <adimonyechidubem@gmail.com>
2024-11-07 12:48:14 +0100

    added game files

---
+++ js/paragraph.js
@@ -0,0 +1,31 @@
+// to compile the paragraphs into an array
+// created paragraph array to store the complete paragraph  
+const paragraphs = [
+    "It had been a rough day. Things hadn't gone as planned and that meant Hannah got yelled at by her boss. It didn't even matter that it wasn't her fault. When things went wrong at work, Hannah got the blame no matter the actual circumstances. It wasn't fair, but there was little she could do without risking her job, and she wasn't in a position to do that with the plans she had.",
+
+    "significant civil There was a time and a place for Stephanie to use her magic. The problem was that she had a difficult time determining this. She wished she could simply use it when the desire hit and there wouldn't be any unforeseen consequences. Unfortunately, that's not how it worked and the consequences could be devastating if she accidentally used her magic at the wrong time,there wouldn't be.",
+
+    "hold executive become station successful enough task exactly reflect about fear let perform term. The truth is, you'll make choice like that every day of your life. The problem is that on most days, you won't know the choice you make will have such a huge impact on your life in the future. So if you want to end up in a certain place in the future, you need to be careful of the choices you make today.",
+
+    "always industry spend feeling play federal performance season major buy ability evidence treat wall Josh had spent year and year accumulating the information. He knew it inside out and if there was ever anyone looking for an expert in the field, Josh would be the one to call. The problem was that there was nobody interested in the information besides him and he knew it.",
+
+    "true like project return popular whether inside especially say size fast really activity final use There once lived an old man and an old woman who were peasants and had to work hard to earn their daily bread. The old man used to go to fix fences and do other odd jobs for the farmers around, and while he was gone the old woman, his wife, did the work of the house and worked in their own little plot of land",
+
+    "strategy maintain see add explain conference school line almost economy rise various claim range She had a terrible habit of comparing her life to others. She realized that their life experiences were completely different than her own and that she saw only what they wanted her to see, but that didn't matter. She still compared herself and yearned for what she thought they had and she didn't",
+
+    "imagine their central watch art right century scientist thought radio rule call administration light The young man wanted a role model. He looked long and hard in his youth, but that role model never materialized. His only choice was to embrace all the people in his life he didn't want to be like her to see, but that didn't matter. She still compared herself and yearned for what she ",
+
+    "concern pick coach make chair suddenly information show rock pretty ready hang finally music cold Samantha wanted to be famous. The problem was that she had never considered all the downsides to actually being famous. Had she taken the time to objectively consider these downsides, she would have never agreed to publically sing that first song. the sunlight so he could begin the day, but he didn't have the energy or willpower. Before ",
+
+    "join professional later though series head college building career consumer everyone sure area maybe The shades were closed keeping the room dark. That he should open them and let in the sunlight so he could begin the day, but he didn't have the energy or willpower. Before and he no longer wanted to spend the energy to begin a new day. He stared at the shades wondering if there was a way to disappear from the reality of the world for the rest of the day.",
+
+    "history wear land matter save realize family plan risk compare prepare simply meet last however Twenty seconds were all that was left and Richard could hear each one tick by. Fifteen seconds now remained and the panic began to fully set in. Ten seconds and he wasn't sure he had enough time. Five seconds, four, three, two, one. area maybe The shades were closed keeping the room dark.",
+
+    "score rest card also movement moment material night reduce these live condition yeah Do you really listen when you are talking with someone? I have a friend who listens in an unforgiving way. She actually takes every word you say as being something important and when you have a friend that listens like that, words take on a whole new meaning and he no longer wanted to spend the energy to begin a new day.",
+
+    "food than morning city speak enjoy laugh teacher cell health well summer player interesting might There was only half a worm in the apple. At first, Judy didn't quite comprehend what this meant. Why would only half a worm be living in an apple she wondered. And then it dawned on her. Judy quickly spit out the bite she had just taken expecting to see the other half of the worm. It ended up being much worse than that.",
+
+    "subject movie themselves price trip address anything million get image probably recent why reveal Stormi is a dog. Her eyes are expressive and are able to let her humans know what she is thinking. Her tongue is long, pink, and wet. Her long legs allow her to sprint after other dogs, people or bunnies. but also very bad. when happy or excited and hides between her back legs when she is bad. Stormi is a dog I love.",
+
+    "billion write hair may remove car response just The computer wouldn't start. She banged on the side and tried again. Nothing. She lifted it up and dropped it to the table. Still nothing. She banged her closed fist against the top. It was at this moment she saw the irony of trying to fix the machine with violence movie themselves price trip address Why would only half a worm be living in an apple she wondered.",
+];
